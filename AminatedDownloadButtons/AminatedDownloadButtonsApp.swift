839075b36068a4f11d59f6435212b4be55f27803
mirohid <mirohid7860@gmail.com>
2025-03-04 11:45:01 +0530

    Initial Commit

---
+++ AminatedDownloadButtons/AminatedDownloadButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AminatedDownloadButtonsApp.swift
+//  AminatedDownloadButtons
+//
+//  Created by MacMini6 on 04/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AminatedDownloadButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
